f37bda4155f494d58f324902ed0ed47cb5ae98c3
ryaoi <ryota@42.codes>
2020-06-10 18:17:42 +0900

    uhm this package should be the latest so I changed the 2.0.2 -> 2.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
 
 setup(
     name='FtApi',
-    version='2.0.2', 
+    version='2.1.2', 
     author="ryaoi",
     author_email="nop@42.codes",
     url="https://github.com/ryaoi/ftApi",
