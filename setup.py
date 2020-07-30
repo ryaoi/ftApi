a2749b1003618dd257e069500aa97c7c6d8178cd
ryota <ryota@42.codes>
2020-07-30 18:53:51 +0900

    update to 2.2.1 to escape from 2.2.0 version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
 
 setup(
     name='FtApi',
-    version='2.2.0', 
+    version='2.2.1', 
     author="ryaoi",
     author_email="nop@42.codes",
     url="https://github.com/ryaoi/ftApi",
